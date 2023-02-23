410d40685b0d6d398a8fdae0d7dc20b659fbfac2
hdp <hdp990624@outlook.com>
2023-02-24 03:24:06 +0800

    Update file chapter_18/ex_03/ex_03.cpp.

--- chapter_18/ex_03/ex_03.cpp
+++ chapter_18/ex_03/ex_03.cpp
@@ -2,7 +2,16 @@
 
 using namespace std;
 
-long double sum_value() { return 0.0; }
+// 下面这种方案行不通：
+// template< typename T, typename... Args >
+// long double sum_value(const T & val, const Args &... args) {
+//     return sizeof...(args) ? val + sum_value(args...) : val;
+// }
+
+template< typename T >
+long double sum_value(const T & val) {
+	return val;
+}
 
 template< typename T, typename... Args >
 long double sum_value(const T & val, const Args &... args) {
@@ -10,7 +19,6 @@ long double sum_value(const T & val, const Args &... args) {
 }
 
 int main(void) {
-	cout << sum_value() << endl;
 	cout << sum_value(3.14) << endl;
 	cout << sum_value(3.14, 17, 'A') << endl;
 	
