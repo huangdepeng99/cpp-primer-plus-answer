ec7828c3537558a4a4041de2df0fc52507f50dd4
hdp <hdp990624@outlook.com>
2023-02-25 00:41:46 +0800

    Update file chapter_18/ex_01/ex_01.cpp.

--- chapter_18/ex_01/ex_01.cpp
+++ chapter_18/ex_01/ex_01.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 #include <algorithm>
+#include <type_traits>
 #include <initializer_list>
 
 using namespace std;
 
 template< typename T >
 T average_list(initializer_list<T> il) {
-	T total_ { };
+	static_assert(is_arithmetic<T>::value,
+				  "error on instantiation, average_list requires arithmetic");
+	long double total_ = 0.0L;
 	for_each(il.begin(), il.end(),
-			 [&total_](const T & x_) { total_ = total_ + x_; });
-	return total_ / il.size();
+			 [&total_](T x_) { total_ += x_; });
+	return static_cast<T> (total_ / il.size());
 }
+
+// 下面是更通用的版本：
+// template< typename T >
+// T average_list(initializer_list<T> il) {
+//     T total_ { };
+//     for_each(il.begin(), il.end(),
+//              [&total_](const T & x_) { total_ = total_ + x_; });
+//     return total_ / static_cast<long double> (il.size());
+// }
 
 int main(void) {
 	auto q = average_list( { 15.4, 10.7, 9.0 } );
